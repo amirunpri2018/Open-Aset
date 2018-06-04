44ac5d16727ba5d8406c63339900789b3410afa3
muntaza <muhammad@muntaza.id>
2018-06-04 11:01:23 +0800

    Penyusutan dengan perhitungan nilai perolehan

---
+++ penyusutan_2017_r2/laporan_penyusutan.sh
@@ -0,0 +1,3 @@
+cat beban_penyusutan_gb_2016.xml | sed -e s/2017/2018/g  | sed -e s/2016/2017/g  | sed -e s/2015/2016/g  > beban_penyusutan_gb_2017.xml
+cat beban_penyusutan_jij_2016.xml | sed -e s/2017/2018/g  | sed -e s/2016/2017/g  | sed -e s/2015/2016/g  > beban_penyusutan_jij_2017.xml
+cat beban_penyusutan_pm_2016.xml | sed -e s/2017/2018/g  | sed -e s/2016/2017/g  | sed -e s/2015/2016/g  > beban_penyusutan_pm_2017.xml
